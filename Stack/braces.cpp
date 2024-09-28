88bd4127b75196db2b21f30bc31ebbfba65fd8c9
Abrorbek Ismoilov <abror2142@gmail.com>
2024-09-28 11:01:48 +0500

    Better approach

--- Stack/braces.cpp
+++ Stack/braces.cpp
@@ -14,18 +14,50 @@ bool is_valid_braces(string s)
     b[']'] = '[';
 
     stack<char> stack;
+    bool is_valid = true;
+
+    // works but not good for long string
+    // for(char a : s)
+    //     if(!stack.empty() && stack.top() == b[a])
+    //         stack.pop();
+    //     else
+    //         stack.push(a);    
+
+    // is_valid = stack.empty();
+
+    // better solution: detects early mismatch, so stops in first mismatch encounter 
     for(char a : s)
-        if(!stack.empty() && stack.top() == b[a])
-            stack.pop();
-        else
-            stack.push(a);    
-    bool is_empty = stack.empty();
+    {
+       if(b[a]){
+        if(stack.empty())
+        {
+            is_valid = false;
+            break;
+        }else {
+            if(stack.top() == b[a])
+            {
+                stack.pop();
+            }else
+            {
+                is_valid = false;
+                break;
+            }
+        }
+       } else 
+       {
+        stack.push(a);
+       }
+    }
+    // valid string would complete pop and push cycle without leaving a char in stack
+    if(!stack.empty())
+        is_valid = false;
+
     cout << "\nStack: " << endl;
     while(!stack.empty()){
         cout << stack.top() << endl;
         stack.pop();
     }
-    return is_empty;
+    return is_valid;
 }
 
 int main(int argc, char * argv[])
