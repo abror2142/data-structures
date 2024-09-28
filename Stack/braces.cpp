94a6aecb95b3b84fc699ebb0a7e80ca72110a2aa
Abrorbek Ismoilov <abror2142@gmail.com>
2024-09-28 11:23:16 +0500

    Another cleaner approach

--- Stack/braces.cpp
+++ Stack/braces.cpp
@@ -2,9 +2,16 @@
 #include <stack>
 #include <string>
 #include <map>
+#include <set>
 
 using namespace std;
 
+bool is_left_bracket(char ch)
+{
+    set<char> left_brackets = {'{', '[', '('};
+    auto it = left_brackets.find(ch);
+    return it != left_brackets.end(); 
+}
 
 bool is_valid_braces(string s)
 {
@@ -25,30 +32,48 @@ bool is_valid_braces(string s)
 
     // is_valid = stack.empty();
 
-    // better solution: detects early mismatch, so stops in first mismatch encounter 
+    // // better solution: detects early mismatch, so stops in first mismatch encounter 
+    // for(char a : s)
+    // {
+    //    if(b[a]){
+    //     if(stack.empty())
+    //     {
+    //         is_valid = false;
+    //         break;
+    //     }else {
+    //         if(stack.top() == b[a])
+    //         {
+    //             stack.pop();
+    //         }else
+    //         {
+    //             is_valid = false;
+    //             break;
+    //         }
+    //     }
+    //    } else 
+    //    {
+    //     stack.push(a);
+    //    }
+    // }
+    // // valid string would complete pop and push cycle without leaving a char in stack
+    // if(!stack.empty())
+    //     is_valid = false;
+
+    // Another cleaner approach
     for(char a : s)
     {
-       if(b[a]){
-        if(stack.empty())
+        if(is_left_bracket(a))
+        {
+            stack.push(a);
+        }else if (stack.empty() || stack.top() != b[a])
         {
             is_valid = false;
             break;
-        }else {
-            if(stack.top() == b[a])
-            {
-                stack.pop();
-            }else
-            {
-                is_valid = false;
-                break;
-            }
+        } else {
+            stack.pop();
         }
-       } else 
-       {
-        stack.push(a);
-       }
     }
-    // valid string would complete pop and push cycle without leaving a char in stack
+
     if(!stack.empty())
         is_valid = false;
 
