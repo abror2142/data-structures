7a23425b179457eb155a5d6be56942c7dea510c6
Abrorbek Ismoilov <abror2142@gmail.com>
2024-10-13 18:34:09 +0500

    AVL Tree remove method implementation

--- BinarySearchTree/AVLTree.py
+++ BinarySearchTree/AVLTree.py
@@ -62,16 +62,11 @@ class AVLTree:
 
         return B
     
-    def _insert(self, node: Node, data) -> Node:
-        if node is None:
-            return Node(data)
-        
-        if data > node.data:
-            node.right = self._insert(node.right, data)
-        if data < node.data:
-            node.left = self._insert(node.left, data)
-        
+    def _updateHeight(self, node: Node) -> None:
         node.height = 1 + max(self.height(node.left), self.height(node.right))
+        return
+    
+    def _balanceNode(self, node: Node, data) -> Node:
 
         balance = self._getBalanceFactor(node)
 
@@ -88,14 +83,72 @@ class AVLTree:
         
         if balance < -1 and data > node.right.data:
             return self._leftRotate(node)
-        
+
         return node
+
+
+    def _insert(self, node: Node, data) -> Node:
+        if node is None:
+            return Node(data)
+        
+        if data > node.data:
+            node.right = self._insert(node.right, data)
+        if data < node.data:
+            node.left = self._insert(node.left, data)
+        
+        # Update height of the node.
+        self._updateHeight(node)
+
+        # Balance the node if it is unbalanced!
+        return self._balanceNode(node, data)
+
+    def _remove(self, node: Node, data) -> Node:
+        if node is None:
+            return None
+        
+        if data > node.data:
+            node.right = self._remove(node.right, data)
+        elif data < node.data:
+            node.left = self._remove(node.left, data)
+        else:
+            # Here node is found!
+            # Should not return here. as there is work to perform below!
+            if node.left is None:
+                temp = node.right
+                node = temp
+            elif node.right is None:
+                temp = node.left
+                node = temp
+            else:
+                # This node has two children
+                successor = self._getRightSuccessor(node)
+                node.data = successor.data
+                node.right = self._remove(node.right, successor.data)
+
+                # If you want to use left node instead, uncomment these lines
+                # successor = self._getLeftSuccessor(data)
+                # node.data = successor.data
+                # self._remove(node.right, successor.data)
+
+        # If the node is None there is no work to do.
+        if node is None:
+            return node
+        
+        # Else Perform the work to keep tree balanced
+        # Work to be done to keep balance of the height among tree nodes;
+
+        # Update the height of the node
+        self._updateHeight(node)
+
+        # Balance the node
+        return self._balanceNode(node, data)
+
 
     def _preOrderTraversal(self, node: Node) -> None:
         if node is None:
             return
-        self._preOrderTraversal(node.left)
         print(node.data)
+        self._preOrderTraversal(node.left)
         self._preOrderTraversal(node.right)
     
     def _find(self, node: Node, data) -> Node:
@@ -119,6 +172,9 @@ class AVLTree:
             return 0
         return node.height
     
+    def contains(self, data):
+        return self._find(self.root, data) is not None
+
     def preOrderTraversal(self) -> None:
         if self.root is None:
             raise KeyError("Tree is empty!")
@@ -137,6 +193,11 @@ class AVLTree:
         else:
             self.root = self._insert(self.root, data)
 
+    def remove(self, data):
+        if not self.contains(data):
+            raise KeyError("There is no key!")
+        self._remove(self.root, data)
+
 
 tree = AVLTree(12)
 
@@ -152,6 +213,10 @@ tree.insert(7)
 tree.insert(5)
 tree.insert(6)
 tree.insert(11)
+tree.remove(1)
+tree.remove(7)
+tree.remove(12)
+tree.remove(6)
 print("Height:", tree.getHeightOf(3))
 
 tree.preOrderTraversal()
